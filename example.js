14fc30a00458278cd33d474b26a090a54f5fc65a
Alex Safonov <me@asafonov.org>
2012-01-18 14:04:43 +0400

    bar plot added

--- example.js
+++ example.js
@@ -1,7 +1,20 @@
+var spam = [];
+for (var i=0; i<40; i++) {
+    spam.push(Math.random());
+}
+
+
 function plot2DExample(canvas_id, stroke_style) {
-    var spam = [];
-    for (var i=0; i<50; i++) {
-	spam.push(Math.random());
-    }
     plot2D(canvas_id, spam, stroke_style);
+}
+
+function printData() {
+    document.write('<p>Data: '+spam[0]);
+    for (var i=1; i<spam.length; i++) {
+	document.write(', '+spam[i]);
+    }
+}
+
+function bar2DExample(canvas_id, stroke_style) {
+    bar2D(canvas_id, spam, stroke_style);
 }
